2b7ac74ce4c7047527ced400e0da48422730b086
Mkisecok <mahmutkisecok@gmail.com>
2022-01-01 18:41:15 +0100

    Add special validation message to validation.js

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,12 +1,12 @@
 import * as yup from 'yup'
 
 const validations = yup.object().shape({
-    firstName: yup.string().min(3).required(),
+    firstName: yup.string().min(3, 'Your name must have min 3 character').required(),
     lastName:yup.string().required(),
     email: yup.string().email().required(),
-    gender:yup.string().oneOf(['male','female','other'],'one of them').required() ,
+    gender:yup.string().oneOf(['male','female','other']).required() ,
     password:yup.string().min(5).required(),
-    passwordConfirm:yup.string().oneOf([yup.ref('password')]).required(),
+    passwordConfirm:yup.string().oneOf([yup.ref('password')],'Thats not a same with your password').required(),
     
     
   });
